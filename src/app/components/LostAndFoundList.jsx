c752f442dc9d9bc78c4affc7fb2500efd018461b
Özgür Ege Şire <ege.sire@ug.bilkent.edu.tr>
2023-10-03 08:59:53 +0300

    Created Lost and Found List Component and Page Endpoint

---
+++ src/app/components/LostAndFoundList.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const LostAndFoundList = ({ items }) => {
+    const listItemStyle = {
+        border: '1px solid #ccc',
+        padding: '10px',
+        borderColor: "black",
+        display:"block",
+        textAlign:"center",
+        background:"white"
+    };
+    const listItemContent = {
+        border: '1px solid #ccc',
+        borderColor: "black",
+        display:"inline-block",
+        width:"33%",
+
+        color:"black"
+    };
+    return (
+        <ul>
+            {items.map((item, index) => (
+                <div key={index} className="list-item" style={listItemStyle}>
+                <div className="listItemContent" style={listItemContent}>
+                    <p>{item.text1}</p>
+                </div>
+                <div className="listItemContent" style={listItemContent}>
+                    <p>{item.text2}</p>
+                </div>
+                <div className="listItemContent" style={listItemContent}>
+                    <p>{item.text3}</p>
+                </div>
+                </div>
+            ))}
+        </ul>
+
+    );
+};
+
+export default LostAndFoundList;
---
+++ src/app/lostfound/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import LostAndFoundList from "../components/LostAndFoundList";
+import Navbar from "../components/Navbar";
+import { useEffect } from 'react';
+
+export default function Login() {
+
+}
