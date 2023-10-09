e75747e1977a1f063e3786534f2012c625a3be37
Serhan <serhanturantr@gmail.com>
2023-10-09 15:58:32 +0300

    Started to Social
    Missing TodayInBilkent
    A few little details to fix

--- src/app/components/SocialPostCard.jsx
+++ src/app/components/SocialPostCard.jsx
@@ -5,12 +5,14 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const SocialPostCard = ({
   sharer,
   title,
-  content
+  content,
+  type,
 }) => {
   return (
     <div className="card bg-white ">
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
+        <p className="card-text">Type: {type}</p>
         <p className="card-text">Sharer: {sharer}</p>
         <p className="card-text">Content: {content}</p>
         <button className="btn btn-primary">See the Post</button>
