a03ef2c5cb03f05e059fae0be64c828d4122fb6a
Umut Can Bolat <umutcanbolat03@gmail.com>
2023-11-21 13:07:37 +0300

    Navbar added to signup page

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import Navbar from "../components/Navbar";
 
 export default function Signup() {
   const [step, setStep] = useState(1); // Step 1: Enter email, Step 2: Enter verification code
@@ -65,91 +66,77 @@ export default function Signup() {
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-        background: "linear-gradient(to bottom, #3498db, #ffffff)",
-      }}
-    >
+    <div>
+      <Navbar />
       <div
         style={{
-          backgroundColor: "#fff",
-          padding: "20px",
-          borderRadius: "5px",
-          boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-          width: "300px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+          background: "linear-gradient(to bottom, #3498db, #ffffff)",
         }}
       >
-        <h2
-          style={{ textAlign: "center", marginBottom: "20px", color: "#333" }}
+        <div
+          style={{
+            backgroundColor: "#fff",
+            padding: "20px",
+            borderRadius: "5px",
+            boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+            width: "300px",
+          }}
         >
-          Sign Up
-        </h2>
-        {step === 1 && (
-          <>
-            <div>
-              <label style={{ color: "black" }}>
-                Email ending with bilkent.edu.tr:
-              </label>
-              <input
-                type="email"
-                placeholder="Enter your email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+          <h2
+            style={{ textAlign: "center", marginBottom: "20px", color: "#333" }}
+          >
+            Sign Up
+          </h2>
+          {step === 1 && (
+            <>
+              <div>
+                <label style={{ color: "black" }}>
+                  Email ending with bilkent.edu.tr:
+                </label>
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  style={{
+                    width: "100%",
+                    padding: "10px",
+                    marginBottom: "10px",
+                    border: "1px solid #ccc",
+                    borderRadius: "3px",
+                    color: "black",
+                  }}
+                />
+              </div>
+              <button
+                type="button"
+                onClick={handleSignup}
                 style={{
                   width: "100%",
                   padding: "10px",
-                  marginBottom: "10px",
-                  border: "1px solid #ccc",
+                  backgroundColor: "#007bff",
+                  color: "#fff",
+                  border: "none",
                   borderRadius: "3px",
-                  color: "black",
+                  cursor: "pointer",
                 }}
-              />
-            </div>
-            <button
-              type="button"
-              onClick={handleSignup}
-              style={{
-                width: "100%",
-                padding: "10px",
-                backgroundColor: "#007bff",
-                color: "#fff",
-                border: "none",
-                borderRadius: "3px",
-                cursor: "pointer",
-              }}
-            >
-              Initiate Verification
-            </button>
-          </>
-        )}
-        {step === 2 && (
-          <>
-            <label style={{ color: "black" }}>Verification Code:</label>
-            <input
-              type="text"
-              placeholder="Enter verification code"
-              value={verificationCode}
-              onChange={(e) => setVerificationCode(e.target.value)}
-              style={{
-                width: "100%",
-                padding: "10px",
-                marginBottom: "10px",
-                border: "1px solid #ccc",
-                borderRadius: "3px",
-                color: "black",
-              }}
-            />
-            <div>
-              <label style={{ color: "black" }}>Username:</label>
+              >
+                Initiate Verification
+              </button>
+            </>
+          )}
+          {step === 2 && (
+            <>
+              <label style={{ color: "black" }}>Verification Code:</label>
               <input
                 type="text"
-                placeholder="Enter your username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Enter verification code"
+                value={verificationCode}
+                onChange={(e) => setVerificationCode(e.target.value)}
                 style={{
                   width: "100%",
                   padding: "10px",
@@ -159,41 +146,58 @@ export default function Signup() {
                   color: "black",
                 }}
               />
-            </div>
-            <div>
-              <label style={{ color: "black" }}>Password:</label>
-              <input
-                type="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+              <div>
+                <label style={{ color: "black" }}>Username:</label>
+                <input
+                  type="text"
+                  placeholder="Enter your username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  style={{
+                    width: "100%",
+                    padding: "10px",
+                    marginBottom: "10px",
+                    border: "1px solid #ccc",
+                    borderRadius: "3px",
+                    color: "black",
+                  }}
+                />
+              </div>
+              <div>
+                <label style={{ color: "black" }}>Password:</label>
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  style={{
+                    width: "100%",
+                    padding: "10px",
+                    marginBottom: "10px",
+                    border: "1px solid #ccc",
+                    borderRadius: "3px",
+                    color: "black",
+                  }}
+                />
+              </div>
+              <button
+                type="button"
+                onClick={handleSignup}
                 style={{
                   width: "100%",
                   padding: "10px",
-                  marginBottom: "10px",
-                  border: "1px solid #ccc",
+                  backgroundColor: "#007bff",
+                  color: "#fff",
+                  border: "none",
                   borderRadius: "3px",
-                  color: "black",
+                  cursor: "pointer",
                 }}
-              />
-            </div>
-            <button
-              type="button"
-              onClick={handleSignup}
-              style={{
-                width: "100%",
-                padding: "10px",
-                backgroundColor: "#007bff",
-                color: "#fff",
-                border: "none",
-                borderRadius: "3px",
-                cursor: "pointer",
-              }}
-            >
-              Complete Registration
-            </button>
-          </>
-        )}
+              >
+                Complete Registration
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
