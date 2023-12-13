a7b187bf458f95a5d026054b4e34221bcdcee2b9
Cenker Akan <cenkerakan@hotmail.com>
2023-12-13 22:39:59 +0300

    Revert "Revert "Revert "Merge branch 'main' into social_page_feed"""
    This reverts commit 3ec0ed7cea2e6466f7b2991bccaa8dd00f0aa6c7.

--- src/app/components/SocialPostCard.jsx
+++ src/app/components/SocialPostCard.jsx
@@ -12,7 +12,6 @@ const SocialPostCard = ({
   title,
   content,
   type,
-  imageURL,
 }) => {
   const [showCommentsModal, setShowCommentsModal] = useState(false);
 
@@ -35,32 +34,7 @@ const SocialPostCard = ({
         }
       `}</style>
 
-
-
-    <div className="card bg-white " style={{ width: '400px' }}>
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">Sharer: {sharer}</p>
-        {imageURL && (
-        <div
-          className="card-img-top"
-          style={{
-            height: "200px", // Set a fixed height for uniformity
-            overflow: "hidden", // Hide overflowing content
-          }}
-        >
-          <img
-            src={imageURL}
-            alt="Product Image"
-            style={{
-              width: "100%", // Ensure the image covers the container width
-              objectFit: "cover", // Crop the image while maintaining aspect ratio
-              maxHeight: "100%", // Ensure the image covers the container height
-            }}
-          />
-        </div>
-      )}
-        <p className="card-text">Content: {content}</p>
+      <div className="card bg-white" style={{ width: '400px' }}>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">Sharer: {sharer}</p>
