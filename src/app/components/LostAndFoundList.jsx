9b6785b07c453be62e5e4727defdedf595f8c959
ozguregesire <ozguregesire@gmail.com>
2023-12-16 09:02:42 +0300

    Implement Lost and Found, Profile page image upload

--- src/app/components/LostAndFoundList.jsx
+++ src/app/components/LostAndFoundList.jsx
@@ -5,35 +5,47 @@ const LostAndFoundList = ({ items }) => {
         border: '1px solid #ccc',
         padding: '10px',
         borderColor: "black",
-        display:"block",
-        textAlign:"center",
-        background:"white"
+        display: "block",
+        textAlign: "center",
+        background: "white"
     };
     const listItemContent = {
         border: '1px solid #ccc',
         borderColor: "black",
-        display:"inline-block",
-        width:"33%",
-        
-        color:"black"
+        display: "inline-block",
+        width: "33%",
+
+        color: "black"
     };
     return (
-        <ul>
+        <>
+            {items?.length &&
+                <div className="list-item" style={listItemStyle}>
+                    <div className="listItemContent" style={listItemContent}>
+                        <b>Founded In</b>
+                    </div>
+                    <div className="listItemContent" style={listItemContent}>
+                        <b>Description</b>
+                    </div>
+                    <div className="listItemContent" style={listItemContent}>
+                        <b>Delivered To</b>
+                    </div>
+                </div>
+            }
             {items.map((item, index) => (
-                <div key={index} className="list-item" style={listItemStyle}>
-                <div className="listItemContent" style={listItemContent}>
-                    <p>{item.text1}</p>
-                </div>
-                <div className="listItemContent" style={listItemContent}>
-                    <p>{item.text2}</p>
-                </div>
-                <div className="listItemContent" style={listItemContent}>
-                    <p>{item.text3}</p>
-                </div>
-                </div>
+                <li key={index} className="list-item" style={listItemStyle}>
+                    <div className="listItemContent" style={listItemContent}>
+                        <p>{item.text1}</p>
+                    </div>
+                    <div className="listItemContent" style={listItemContent}>
+                        <p>{item.text2}</p>
+                    </div>
+                    <div className="listItemContent" style={listItemContent}>
+                        <p>{item.text3}</p>
+                    </div>
+                </li>
             ))}
-        </ul>
-        
+        </>
     );
 };
 
