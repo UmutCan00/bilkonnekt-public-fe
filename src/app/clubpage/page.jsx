5cbe9819a02cca8b175957eb300532893f5e062f
Umut Can Bolat <umutcanbolat03@gmail.com>
2023-12-17 21:23:04 +0300

    Remove empty button

--- src/app/clubpage/page.jsx
+++ src/app/clubpage/page.jsx
@@ -7,13 +7,13 @@ import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 //import clubData from '../mockdata/clubData';
 
-import Link from 'next/link';
+import Link from "next/link";
 import "bootstrap/dist/css/bootstrap.css";
-import { Modal } from 'react-bootstrap'; // Import Bootstrap modal
+import { Modal } from "react-bootstrap"; // Import Bootstrap modal
 import "../globals.css";
-let count=1;
+let count = 1;
 const ClubsPage = () => {
-  const { data: session } = useSession(); 
+  const { data: session } = useSession();
   const token = session?.backendTokens?.accessToken;
   const [showModal, setShowModal] = useState(false);
   const [selectedClub, setSelectedClub] = useState({});
@@ -25,18 +25,21 @@ const ClubsPage = () => {
     setShowModal(true);
     //console.log("sd",selectedClub);
   };
- 
-  useEffect( () => {
+
+  useEffect(() => {
     // This effect runs whenever selectedClub changes
     const fetchClubs = async () => {
       try {
-      const response = await fetch("http://localhost:3500/api/social/getClubs", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const response = await fetch(
+          "http://localhost:3500/api/social/getClubs",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
         if (response.ok) {
           const data = await response.json();
           console.log(data);
@@ -45,62 +48,104 @@ const ClubsPage = () => {
           console.error("Failed to fetch data");
         }
       } catch (error) {
-        console.log("get all clubs operation error: ", error)
+        console.log("get all clubs operation error: ", error);
       }
-    }
+    };
     fetchClubs();
     console.log("Selected Club in effect:", selectedClub);
   }, [selectedClub]);
 
-    return (
-      <div>
-        <Navbar />
-        
-        <div className="container">
-        <h1 className="card text-center titleColor text-white" style={{ marginLeft: '33%',fontSize:"20px",maxWidth:"500px" }}>Student Clubs</h1>
-        <div style={{ marginLeft: '15%',maxWidth:"1000px" }}>
-        <ul className="card m-2 titleColor " style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '0px' }}>
-        {clubData.map((club) => (
-          
-            <li className="card m-2" key={club.id} style={{ marginTop: '30px', marginLeft:'0px' }}>
+  return (
+    <div>
+      <Navbar />
+
+      <div className="container">
+        <h1
+          className="card text-center titleColor text-white"
+          style={{ marginLeft: "33%", fontSize: "20px", maxWidth: "500px" }}
+        >
+          Student Clubs
+        </h1>
+        <div style={{ marginLeft: "15%", maxWidth: "1000px" }}>
+          <ul
+            className="card m-2 titleColor "
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(3, 1fr)",
+              gap: "0px",
+            }}
+          >
+            {clubData.map((club) => (
+              <li
+                className="card m-2"
+                key={club.id}
+                style={{ marginTop: "30px", marginLeft: "0px" }}
+              >
                 <p>{club.name}</p>
-                {/* <Link href={`/clubdetailspage/${club.id}`}> */} 
-                <img src={club.imageURL} alt={club.name} onClick={() => handleClubClick(club)} />
-            
-            </li>
-             
-          ))}
+                {/* <Link href={`/clubdetailspage/${club.id}`}> */}
+                <img
+                  src={club.imageURL}
+                  alt={club.name}
+                  onClick={() => handleClubClick(club)}
+                />
+              </li>
+            ))}
           </ul>
-          <Modal show={showModal} onHide={() => setShowModal(false)} style={{ color: "black" }}>
-          <Modal.Header closeButton>
-            <Modal.Title style={{ color: "black" }}>{ selectedClub?.name}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div>
-            <img src={selectedClub.imageURL} alt={selectedClub.name} />
-            <h1>Club Bio goes here: {selectedClub.description}</h1>
-    </div>
-    <div> <button type="button" className="btn btn-success" style={{ color: "black" }} onClick={() => setShowModal(false)}>Join Club</button> </div>
-    <div><button type="button" className="btn btn-danger" style={{ color: "black" }} onClick={() => setShowModal(false)}>Edit Club Page</button></div>
-    <div><button type="button" className="btn btn-primary" style={{ color: "black" }} onClick={() => setShowModal(false)}> <div>
-    <Link href={`/clubdetailspage/${selectedClub._id}`}>Go To Club Profile</Link>
-    </div></button></div>
-          </Modal.Body>
-          <Modal.Footer>
-            <button onClick={() => setShowModal(false)}>Close</button>
-          </Modal.Footer>
-        </Modal>
-      </div>
+          <Modal
+            show={showModal}
+            onHide={() => setShowModal(false)}
+            style={{ color: "black" }}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title style={{ color: "black" }}>
+                {selectedClub?.name}
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div>
+                <img src={selectedClub.imageURL} alt={selectedClub.name} />
+                <h1>Club Bio goes here: {selectedClub.description}</h1>
+              </div>
+              <div>
+                {" "}
+                <button
+                  type="button"
+                  className="btn btn-success"
+                  style={{ color: "black" }}
+                  onClick={() => setShowModal(false)}
+                >
+                  Join Club
+                </button>{" "}
+              </div>
+              <div>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  style={{ color: "black" }}
+                  onClick={() => setShowModal(false)}
+                >
+                  {" "}
+                  <div>
+                    <Link href={`/clubdetailspage/${selectedClub._id}`}>
+                      Go To Club Profile
+                    </Link>
+                  </div>
+                </button>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <button onClick={() => setShowModal(false)}>Close</button>
+            </Modal.Footer>
+          </Modal>
+        </div>
       </div>
       <style jsx>{`
-      .titleColor{
-        background-color: #0B1356;
-      }
-      
-      
+        .titleColor {
+          background-color: #0b1356;
+        }
       `}</style>
-      </div>
-    );
-  };
-  
-  export default ClubsPage;
+    </div>
+  );
+};
+
+export default ClubsPage;
